19cdf300d60a4dccde1976ceb3540db0e02cd9b8
BigodeSan <40863800+BigodeSan@users.noreply.github.com>
2018-08-02 15:11:39 -0300

    up

--- js/script.js
+++ js/script.js
@@ -7,15 +7,17 @@ $(function($){
         url: "https://formspree.io/sensualmdintima@gmail.com", 
         method: "POST",
         data: {
-            name: $("#name").val(),
+            name: $("#nome").val(),
             email: $("#email").val(),
-            message: $("#message").val(),
+            telefone: $("#telefone").val(),
+            message: $("#mensagem").val(),
         },
         dataType: "json"
         }).done(function(){
-                $("#name").val("");
+                $("#nome").val("");
                 $("#email").val("");
-                $("#message").val("");
+                $("#telefone").val(""),    
+                $("#mensagem").val("");
                 alert("Email enviado com sucesso!");
         }).fail(function(){
                 alert("Não foi possivel enviar seu email, tente mais tarde!");
