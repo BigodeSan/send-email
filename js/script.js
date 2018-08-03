3295b095f04c463d10cdf71ce2bd9f832a60e10d
BigodeSan <40863800+BigodeSan@users.noreply.github.com>
2018-08-02 21:28:06 -0300

    up

--- js/script.js
+++ js/script.js
@@ -1,38 +1,28 @@
 $(function($){
-    $("form").submit(function(event){
-        
-        event.preventDefault();
-        
-        $.ajax({
-        url: "https://formspree.io/sensualmdintima@gmail.com", 
-        method: "POST",
-        data: {
-            name: $("#nome").val(),
-            email: $("#email").val(),
-            telefone: $("#telefone").val(),
-            message: $("#mensagem").val(),
-        },
-        dataType: "json"
-        },
-                
-                cache: false,
-                success: function()  {
-                    // Success message
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-success')
-                        .append("<strong>Sua Mensagem foi enviada. </strong>");
-                    $('#success > .alert-success')
-                        .append('</div>');
 
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                })
-        }).fail(function(){
-                alert("Não foi possivel enviar seu email, tente mais tarde!");
-        });
-        
-    });
-    
-});
+	$("form").submit(function(event) {
+
+		event.preventDefault();
+
+		$.ajax({
+	    url: "https://formspree.io/sensualmdintima@gmail.com", 
+	    method: "POST",
+	    data: {
+	    	name: $("#nome").val(),
+	    	email: $("#email").val(),
+            telefone: $("telefone").val(),
+	    	message: $("#mensagem").val()
+	    },
+	    dataType: "json"
+		}).done(function send(){
+			$("#nome").val("");
+			$("#email").val("");
+            $("#telefone").val("");
+			$("#mensagem").val("");
+			alert("Email enviado com sucesso!!!");
+		}).fail(function(){
+			alert("Erro ao enviar email!");
+		});
+	});
+
+}) ;
