38b80aeb1e51689fe033a8f446eeb62f81e4555e
BigodeSan <40863800+BigodeSan@users.noreply.github.com>
2018-08-04 20:03:47 -0300

    up

--- js/script.js
+++ js/script.js
@@ -8,22 +8,28 @@ $(function($){
 	    url: "https://formspree.io/sensualmdintima@gmail.com", 
 	    method: "POST",
 	    data: {
-	    	name: $("#nome").val(),
+	    	name: $("#name").val(),
 	    	email: $("#email").val(),
-            telefone: $("telefone").val(),
-	    	message: $("#mensagem").val()
+	    	telefone: $("#telefone").val(),
+            message: $("#message").val()
 	    },
 	    dataType: "json"
 		}).done(function(){
-			$('#send').html("<div class='alert alert-danger'>");
-            		$('#send > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-            		.append("</button>");
-         		 $('#send > .alert-danger').append($("<strong>").text("Email enviado com sucesso!"));
-          		$('#send > .alert-danger').append('</div>');
+			$("#send").html("<div class='alert alert-success'>");
+            $("#send > .alert-success").html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+            .append("</button>");
+            $("#send > .alert-success").append($("<strong>").text("Email enviado com sucesso! retornaremos em breve."));
+            $("#send > .alert-success").append('</div>');
             
-           	 $('#form').trigger("reset");
+            $('#form').trigger("reset");
 		}).fail(function(){
-			alert("Erro ao enviar email!");
+			$("#send").html("<div class='alert alert-danger'>");
+            $("#send > .alert-danger").html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+            .append("</button>");
+            $("#send > .alert-danger").append($("<strong>").text("Desculpe, não foi possivel enviar seu email. Tente mais tarde!"));
+            $("#send > .alert-danger").append('</div>');
+            
+            $('#form').trigger("reset");
 		});
 	});
 
