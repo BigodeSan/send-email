297fb2a233f9e47062c48ebc29ac8f4282b3189b
BigodeSan <40863800+BigodeSan@users.noreply.github.com>
2018-08-05 19:01:02 -0300

    up

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$(function($){
+/* $(function($){
 
 	$("form").submit(function(event) {
 
@@ -30,6 +30,45 @@ $(function($){
             $("#send > .alert-danger").append('</div>');
             
             $('#form').trigger("reset");
+		});
+	});
+
+}) ; */
+
+$(function($){
+
+	$("form").submit(function(event) {
+
+		event.preventDefault();
+
+		$.ajax({
+	    url: "https://formspree.io/sensualmdintima@gmail.com", 
+	    method: "POST",
+	    data: {
+	    	nome: $("#nome").val(),
+	    	email: $("#email").val(),
+	    	telefone: $("#telefone").val(),
+            mensagem: $("#mensagem").val()
+	    },
+	    dataType: "json"
+		}).done(function(){
+			$("#send").html("<div class='alert alert-success'>");
+            $("#send > .alert-success").html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+            .append("</button>");
+            $("#send > .alert-success").append($("<strong>").text("Email enviado com sucesso! retornaremos em breve."));
+            $("#send > .alert-success").append('</div>');
+            
+            $('#form').trigger("reset");
+            
+		}).fail(function(){
+			$("#send").html("<div class='alert alert-danger'>");
+            $("#send > .alert-danger").html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+            .append("</button>");
+            $("#send > .alert-danger").append($("<strong>").text("Desculpe, não foi possivel enviar seu email. Tente mais tarde!"));
+            $("#send > .alert-danger").append('</div>');
+            
+            $('#form').trigger("reset");
+            
 		});
 	});
 
