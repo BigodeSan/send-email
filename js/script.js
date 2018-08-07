69b3d1c60cdb1d7e505a2f3f8b0b0297eb11fbf0
BigodeSan <40863800+BigodeSan@users.noreply.github.com>
2018-08-07 10:42:07 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -26,7 +26,7 @@
 			$("#send").html("<div class='alert alert-danger'>");
             $("#send > .alert-danger").html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
-            $("#send > .alert-danger").append($("<strong>").text("Desculpe, não foi possivel enviar seu email. Tente mais tarde!"));
+            $("#send > .alert-danger").append($("<strong>").text("Desculpe, não foi possivel enviar seu email."));
             $("#send > .alert-danger").append('</div>');
             
             $('#form').trigger("reset");
