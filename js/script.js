1be7f7e8a24bd1e41dec9d0485b4dc26e6b9cfe6
BigodeSan <40863800+BigodeSan@users.noreply.github.com>
2018-08-07 12:26:56 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@
 		event.preventDefault();
 
 		$.ajax({
-	    url: "https://formspree.io/claudomiromonteiro@gmail.com", 
+	    url: "https://formspree.io/sensualmdintima@gmail.com", 
 	    method: "POST",
 	    data: {
 	    	nome: $("#nome").val(),
@@ -18,7 +18,7 @@
 			$("#send").html("<div class='alert alert-success'>");
             $("#send > .alert-success").html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
-            $("#send > .alert-success").append($("<strong>").text("Email enviado com sucesso!!!"));
+            $("#send > .alert-success").append($("<strong>").text("Email enviado com sucesso!!"));
             $("#send > .alert-success").append('</div>');
             
             $('#form').trigger("reset");
@@ -26,7 +26,7 @@
 			$("#send").html("<div class='alert alert-danger'>");
             $("#send > .alert-danger").html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
-            $("#send > .alert-danger").append($("<strong>").text("Não foi possivel enviar seu email!"));
+            $("#send > .alert-danger").append($("<strong>").text("Não foi possivel enviar seu email!!"));
             $("#send > .alert-danger").append('</div>');
             
             $('#form').trigger("reset");
