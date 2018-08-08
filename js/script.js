f57678b35027a007241e585a712672c3e8bd4f45
BigodeSan <40863800+BigodeSan@users.noreply.github.com>
2018-08-08 12:11:31 -0300

    up

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ $(function($){
 			$("#send").html("<div class='alert alert-success'>");
             $("#send > .alert-success").html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
-            $("#send > .alert-success").append($("<strong>").text("Email enviado com sucesso! retornaremos em breve."));
+            $("#send > .alert-success").append($("<strong>").text("Email enviado com sucesso! Retornaremos em breve."));
             $("#send > .alert-success").append('</div>');
             
             $('#form').trigger("reset");
