a131be001046653940ee97c3539027c567e3a9f6
BigodeSan <40863800+BigodeSan@users.noreply.github.com>
2018-08-13 20:41:17 -0300

    up

--- js/script.js
+++ js/script.js
@@ -1,25 +1,11 @@
-(function($) {
-    'use strict';
+$(function($){
 
-    $("form").on('submit', function(e) {
-        e.preventDefault();
+	$("form").submit(function(event) {
 
-        $.ajax({
-            url: "https://formspree.io/sensualmdintima@gmail.com", 
-            method: "POST",
-            data: {
-                nome: $("#nome").val(),
-                email: $("#email").val(),
-                telegone: $("#telefone").val(),
-                mensagem: $("#mensagem").val()
-            },
-            dataType: "json"
-        }).done(function(result) {
-            console.log(result); // < DEBUG
+		event.preventDefault();
 
-            $("#send").html("<div class='alert alert-success'>");
 		$.ajax({
-	    url: "https://simple-form.com/sensualmdintima@gmail.com", 
+	    url: "https://formspree.io/sensualmdintima@gmail.com", 
 	    method: "POST",
 	    data: {
 	    	Nome: $("#nome").val(),
@@ -36,21 +22,15 @@
             $("#send > .alert-success").append('</div>');
             
             $('#form').trigger("reset");
-        }).fail(function(err) {
-            console.log(err); // << DEBUG
-
-            $("#send").html("<div class='alert alert-danger'>");
+		}).fail(function(){
+			$("#send").html("<div class='alert alert-danger'>");
             $("#send > .alert-danger").html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
-            $("#send > .alert-danger").append($("<strong>").text("Desculpe, não foi possivel enviar seu email. Tente mais tarde!!!"));
+            $("#send > .alert-danger").append($("<strong>").text("Desculpe, não foi possivel enviar seu email. Tente mais tarde!"));
             $("#send > .alert-danger").append('</div>');
             
             $('#form').trigger("reset");
-        });
-    });
-
-
-})(jQuery);
+		});
+	});
 
 }) ;
-
