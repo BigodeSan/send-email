966ab19f17814918645ba1ddc2f3cb1b20282908
BigodeSan <40863800+BigodeSan@users.noreply.github.com>
2018-08-13 20:07:00 -0300

    upjs

--- js/script.js
+++ js/script.js
@@ -1,36 +1,40 @@
-$(function($){
+(function($) {
+    'use strict';
 
-	$("form").submit(function(event) {
+    $("form").on('submit', function(e) {
+        e.preventDefault();
 
-		event.preventDefault();
+        $.ajax({
+            url: "https://formspree.io/sensualmdintima@gmail.com", 
+            method: "POST",
+            data: {
+                nome: $("#nome").val(),
+                email: $("#email").val(),
+                telegone: $("#telefone").val(),
+                mensagem: $("#mensagem").val()
+            },
+            dataType: "json"
+        }).done(function(result) {
+            console.log(result); // < DEBUG
 
-		$.ajax({
-	    url: "https://formspree.io/sensualmdintima@gmail.com", 
-	    method: "POST",
-	    data: {
-	    	Nome: $("#nome").val(),
-	    	Email: $("#email").val(),
-	    	Telefone: $("#telefone").val(),
-            Mensagem: $("#mensagem").val()
-	    },
-	    dataType: "json"
-		}).done(function(){
-			$("#send").html("<div class='alert alert-success'>");
+            $("#send").html("<div class='alert alert-success'>");
             $("#send > .alert-success").html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
             $("#send > .alert-success").append($("<strong>").text("Email enviado com sucesso! Retornaremos em breve."));
             $("#send > .alert-success").append('</div>');
             
             $('#form').trigger("reset");
-		}).fail(function(){
-			$("#send").html("<div class='alert alert-danger'>");
+        }).fail(function(err) {
+            console.log(err); // << DEBUG
+
+            $("#send").html("<div class='alert alert-danger'>");
             $("#send > .alert-danger").html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
-            $("#send > .alert-danger").append($("<strong>").text("Desculpe, não foi possivel enviar seu email. Tente mais tarde!"));
+            $("#send > .alert-danger").append($("<strong>").text("Desculpe, não foi possivel enviar seu email. Tente mais tarde!!!"));
             $("#send > .alert-danger").append('</div>');
             
             $('#form').trigger("reset");
-		});
-	});
+        });
+    });
 
-}) ;
+})(jQuery);
