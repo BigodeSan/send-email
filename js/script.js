d377ef04f69b0c11144347891df09ec4b985dc15
BigodeSan <40863800+BigodeSan@users.noreply.github.com>
2018-08-13 13:08:35 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ $(function($){
 		event.preventDefault();
 
 		$.ajax({
-	    url: "https://formspree.io/sensualmdintima@gmail.com", 
+	    url: "https://simple-form.com/sensualmdintima@gmail.com", 
 	    method: "POST",
 	    data: {
 	    	Nome: $("#nome").val(),
@@ -26,7 +26,7 @@ $(function($){
 			$("#send").html("<div class='alert alert-danger'>");
             $("#send > .alert-danger").html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
-            $("#send > .alert-danger").append($("<strong>").text("Desculpe, não foi possivel enviar seu email. Tente mais tarde!"));
+            $("#send > .alert-danger").append($("<strong>").text("Desculpe, não foi possivel enviar seu email. Tente mais tarde!!!"));
             $("#send > .alert-danger").append('</div>');
             
             $('#form').trigger("reset");
