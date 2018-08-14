bad7fe6d2807bdd9ef137643133f1fd76375c380
BigodeSan <40863800+BigodeSan@users.noreply.github.com>
2018-08-14 10:48:48 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,24 +8,29 @@ $(function($){
 			url: "https://simple-form.com/sensualmdintima@gmail.com?json",
 			 method: "POST",
 			 data: {
-			name: $("#name").val(),
+			nome: $("#nome").val(),
 			email: $("#email").val(),
-			message: $("#message").val()
+			mensagem: $("#mensagem").val()
 	    	},
 	    	dataType: "json"
 		}).done(function(){
-			$("#name").val("");
-			$("#email").val("");
-			$("#message").val("");
-			alert("Email enviado com sucesso!!");
+			$("#send").html("<div class='alert alert-success'>");
+			$("#send > .alert-success").html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+			.append("</button>");
+			$("#send > .alert-success").append($("<strong>").text("Email enviado com sucesso! Retornaremos em breve."));
+			$("#send > .alert-success").append('</div>');
+
+			$('#form').trigger("reset");
+			
 		}).fail(function(){
-			$("#mensagem").html("<div class='alert alert-danger'>");
-           $("#mensagem > .alert-danger").html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-            .append("</button>");
-          $("#mensagem > .alert-danger").append($("<strong>").text("Desculpe, não foi possivel enviar seu email. Tente mais tarde!"));
-          $("#mensagem > .alert-danger").append('</div>');
+			$("#send").html("<div class='alert alert-danger'>");
+			$("#send > .alert-danger").html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+			.append("</button>");
+			$("#send > .alert-danger").append($("<strong>").text("Desculpe, não foi possivel enviar seu email. Tente mais tarde!"));
+			$("#send > .alert-danger").append('</div>');
             
             $('#form').trigger("reset");
+			
 		});
 	});
 
