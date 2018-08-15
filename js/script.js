b4bfca7662e5593f7b2a5c80d2901e38a8481fd4
BigodeSan <40863800+BigodeSan@users.noreply.github.com>
2018-08-14 21:15:04 -0300

    up

--- js/script.js
+++ js/script.js
@@ -6,28 +6,27 @@ $(function($){
 
 		$.ajax({
 			url: "https://simple-form.com/sensualmdintima@gmail.com?json",
-			 method: "POST",
-			 data: {
+			method: "POST",
+			data: {
 			nome: $("#nome").val(),
 			email: $("#email").val(),
 			telefone: $("#telefone").val(),
 			mensagem: $("#mensagem").val()
-	    	},
-	    	
-		}).done(function(result){
-			
-			console.log(result); // < DEBUG
+	    },
+	    dataType: "json",
+            
+		}).done(function(){
+					
 			$("#send").html("<div class='alert alert-success'>");
 			$("#send > .alert-success").html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
 			.append("</button>");
-			$("#send > .alert-success").append($("<strong>").text("Email enviado com sucesso!! Retornaremos em breve!!"));
+			$("#send > .alert-success").append($("<strong>").text("Email enviado com sucesso. Responderemos o mais breve possivel."));
 			$("#send > .alert-success").append('</div>');
 
 			$('#form').trigger("reset");
 			
-		}).fail(function(err){
+		}).fail(function(){
 			
-			console.log(err); // < DEBUG
 			$("#send").html("<div class='alert alert-danger'>");
 			$("#send > .alert-danger").html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
 			.append("</button>");
